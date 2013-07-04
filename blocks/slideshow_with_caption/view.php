e9e14cd17e44281f85797fcf9b13fe3c154685c8
Justen Doherty <phpchap@gmail.com>
2013-07-04 22:11:57 +0100

    latest round of changes

--- blocks/slideshow_with_caption/view.php
+++ blocks/slideshow_with_caption/view.php
@@ -1,9 +1,71 @@
-<?php  defined('C5_EXECUTE') or die("Access Denied.");
-?>
+<?php  defined('C5_EXECUTE') or die("Access Denied.");?>
+<style>
+.wrapper 
+{
+  margin: 50px auto;
+  width: 250px;
+  height: 370px;
+  background: white;
+  border-radius: 10px;
+  -webkit-box-shadow: 0px 0px 8px rgba(0,0,0,0.3);
+  -moz-box-shadow:    0px 0px 8px rgba(0,0,0,0.3);
+  box-shadow:         0px 0px 8px rgba(0,0,0,0.3);
+  position: relative;
+  z-index: 1;
+}
+.ribbon-outer
+{
+width:143px;
+height:143px;
+overflow:hidden; 
+position:absolute;
+top: 0;
+left: 0;
+z-index:999;
+}
+.ribbon-inner {
+font: bold 18px Sans-Serif;
+background: #00B3DE;
+background: -moz-linear-gradient(top, rgba(0, 179, 222, 1) 0%, rgba(0, 179, 222, 1) 100%);
+background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(0, 179, 222, 1)), color-stop(100%,rgba(0, 179, 222, 1)));
+background: -webkit-linear-gradient(top, rgba(0, 179, 222, 1) 0%,rgba(0, 179, 222, 1) 100%);
+background: -o-linear-gradient(top, rgba(0, 179, 222, 1) 0%,rgba(0, 179, 222, 1) 100%);
+background: -ms-linear-gradient(top, rgba(0, 179, 222, 1) 0%,rgba(0, 179, 222, 1) 100%);
+background: linear-gradient(to bottom, rgba(0, 179, 222, 1) 0%,rgba(0, 179, 222, 1) 100%);
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#ffaf4b', endColorstr='#ff920a',GradientType=0 );
+text-align: center;
+color: white;
+top: 43px;
+left: -40px;
+width: 184px;
+padding: 5px;
+position: relative;
+-webkit-transform: rotate(-45deg);
+}
+.ribbon-inner:before, .ribbon-inner:after {
+  content: "";
+  border-top:   3px solid #CC7A29;   
+  border-left:  3px solid transparent;
+  border-right: 3px solid transparent;
+  position:absolute;
+  bottom: 3px;
+}
+
+.ribbon-inner:before {
+  left: 0;
+}
+.ribbon-inner:after {
+  right: 0;
+}
+</style>
 
 <?php if(!empty($field_2_image) && !empty($field_4_image) && !empty($field_6_image)) { ?>
     <div id="myCarousel" class="carousel slide" style="margin-top:20px">
         <div class="carousel-inner">
+
+
+<div class="ribbon-outer">
+    <div class="ribbon-inner">Rooms from £15</div></div>
             
             <div class="item active">
                 <?php  if (!empty($field_2_image)): ?>
--- themes/atlas_hostel/elements/three_col_right_sidebar.php
+++ themes/atlas_hostel/elements/three_col_right_sidebar.php
@@ -1,32 +1,13 @@
         <div class="span3">
-            <?php /*            
-            <div class="headline"><h3>Best Attractions</h3></div>
-            <div class="posts margin-bottom-20">
-                <dl class="dl-horizontal">
-                    <dt><a href="#"><img src="<?= $this->getThemePath(); ?>/assets/img/sliders/elastislide/6.jpg" alt=""></a></dt>
-                    <dd>
-                        <p><a href="#">London Zoo</a></p> 
-                    </dd>
-                </dl>
-                <dl class="dl-horizontal">
-                    <dt><a href="#"><img src="<?= $this->getThemePath(); ?>/assets/img/sliders/elastislide/10.jpg" alt=""></a></dt>
-                    <dd>
-                        <p><a href="#">Kensington Palace</a></p> 
-                    </dd>
-                </dl>
-                <dl class="dl-horizontal">
-                    <dt><a href="#"><img src="<?= $this->getThemePath(); ?>/assets/img/sliders/elastislide/11.jpg" alt=""></a></dt>
-                    <dd>
-                        <p><a href="#">Tate Modern.</a></p> 
-                    </dd>
-                </dl>
-            </div>
-            */ ?>
             <?php 
             $a = new Area("Right Top");
             $a->display($c);            
             ?>
             <div class="headline"><h3>Location</h3></div>
+                <ul class="unstyled who margin-bottom-20">
+                    <li><i class="icon-home"></i>16 Talbot Square, London, Paddington, W25LH</li>
+                    <li><i class="icon-phone-sign"></i>0845 519 9448</li>
+                </ul>            
             <div style="border:2px solid #AB0765" id="map" class="sidebar map margin-bottom-40"></div>
             <div class="headline"><h3>Directions</h3></div>            
             <div style="display:block; width:187px; height:222px;font-family:Verdana, Arial, Helvetica, sans-serif !important;background:url(http://www.tfl.gov.uk/tfl/gettingaround/journeyplanner/banners/images/widget-panel.gif) #fff no-repeat; font-size:11px;	border: 1px solid #103994;	border-radius: 4px;	box-shadow: 2px 2px 3px 1px #ccc;"><div style="display:block;padding:30px 0 15px 0;"><h2 style="color: rgb(36, 66, 102); text-align: center; display: block; font-size: 15px; font-family: arial; border: 0; margin-bottom: 1em; margin-top: 0;  font-weight: bold !important; background:0; padding: 0">Journey Planner</h2><form action="http://journeyplanner.tfl.gov.uk/user/XSLT_TRIP_REQUEST2" id="jpForm" method="post" target="tfl" style="margin:5px 0 0 0 !important;padding:0 !important;"><input type="hidden" name="language" value="en" /> <!-- in language = english --><input type="hidden" name="execInst" value="" /><input type="hidden" name="sessionID" value="0" /> <!-- to start a new session on JP the sessionID has to be 0 --><input type="hidden" name="ptOptionsActive" value="-1" /> <!-- all pt options are active --><input type="hidden" name="place_origin" value="London" /> <!-- London is a hidden parameter for the origin location --><input type="hidden" name="place_destination" value="London" /><div style="padding-right: 15px; padding-left: 15px"><input type="text"  name="name_origin" style="width:155px !important; padding: 1px" value="From"/><select style="width:155px !important; margin: 0 !important;" name="type_origin"><option value="stop">Station or stop</option><option value="locator">Postcode</option><option value="address">Address</option><option value="poi">Place of interest</option></select></div><input type="hidden" name="name_destination" value="w2 1tr"/><input name="type_destination" value="locator" type="hidden" /><div style="background: url(http://www.tfl.gov.uk/tfl/gettingaround/journeyplanner/banners/images/panel-separator.gif) no-repeat bottom; padding-bottom: 2px;  padding-top: 2px; overflow: hidden; margin-bottom: 8px"><div style="clear: both; background: url(http://www.tfl.gov.uk/tfl-global/images/options-icons.gif) no-repeat 9.5em 0; height: 30px; padding-right: 15px; padding-left: 15px"><a style="text-decoration:none; color:#113B92;font-size: 11px;white-space:nowrap; display: inline-block; padding: 4px 0 5px 0; width: 155px; margin-top: 5px" target="tfl" href="http://journeyplanner.tfl.gov.uk/user/XSLT_TRIP_REQUEST2?language=en&amp;ptOptionsActive=1"  onclick="javascript:document.getElementById('jpForm').ptOptionsActive.value='1';document.getElementById('jpForm').execInst.value='readOnly';document.getElementById('jpForm').submit(); return false">More options</a></div></div><div style="text-align: center;"><input type="submit" title="Leave now" value="Leave now"   style="border-style: none; background-color: #157DB9; display: inline-block; padding: 4px 11px; color: #fff; text-decoration: none; -moz-border-radius: 3px; -webkit-border-radius: 3px;  border-radius: 3px;	-moz-box-shadow: 0 1px 3px rgba(0,0,0,0.25);	-webkit-box-shadow: 0 1px 3px rgba(0,0,0,0.25); box-shadow: 0 1px 3px rgba(0,0,0,0.25); text-shadow: 0 -1px 1px rgba(0,0,0,0.25);	border-bottom: 1px solid rgba(0,0,0,0.25);	position: relative; cursor: pointer;  font: bold  13px/1 Arial,Helvetica,sans-serif; text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.4); line-height: 1;"/></div></form></div></div>                        
