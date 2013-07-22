19b8cd6e68deec3366d0df44e0f9ba01d8ba923a
Justen Doherty <phpchap@gmail.com>
2013-07-22 11:16:27 +0100

    fixed the booking button

--- blocks/book_now/view.php
+++ blocks/book_now/view.php
@@ -3,5 +3,5 @@
     <?php if (!empty($field_1_textbox_text)): ?>
         <h4><?php echo htmlentities($field_1_textbox_text, ENT_QUOTES, APP_CHARSET); ?></h4>
     <?php endif; ?>    
-    <img src="https://myallocator.com/img/booknow/book_button_xl.png" onclick="bookNowApp('s7P1CEHxcx_YaH7HNC092w')" style="cursor:pointer" />
+    <img src="https://myallocator.com/img/booknow/book_button_xl.png" onclick="bookNowApp('zSVbX9tA3z4-AxtKPKVmcg')" style="cursor:pointer" />
 </div>
